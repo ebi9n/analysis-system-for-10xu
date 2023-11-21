79f06cac9066eb9b9d47c6748f633b0613164a76
ebi9n <shrimp15832@gmail.com>
2023-11-21 18:50:53 +0900

    大雑把に実装完了。plotConfigを追加した。

---
+++ plotConfig.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Times New Roman' # font familyの設定
+plt.rcParams['mathtext.fontset'] = 'stix' # math fontの設定
+plt.rcParams["font.size"] = 12 # 全体のフォントサイズが変更されます。
+plt.rcParams['xtick.labelsize'] = 12 # 軸だけ変更されます。
+plt.rcParams['ytick.labelsize'] = 12 # 軸だけ変更されます
--- plotDistColorMapControl.py
+++ plotDistColorMapControl.py
@@ -3,10 +3,14 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotConfig
 
-FIGSIZE = (10,10)
+FIGSIZE = (8,10)
 DPI = 100
 FACECOLOR = "white"
+LASER_RANGE_COLOR = "white"
+LASER_RANGE_ALPHA = 0.50
+CLOSE_SHOW_DIST_PIXEL = 30
 class plotDistColorMapControl:
     """
     カラーマップのプロットを実施
@@ -20,31 +24,64 @@ class plotDistColorMapControl:
         self.fig = plt.figure(figsize=FIGSIZE, dpi=DPI,facecolor=FACECOLOR)
         # 座標軸の作成
         self.ax_main = self.fig.add_subplot(211)
-        self.ax_up = self.fig.add_subplot(223)
-        self.ax_down = self.fig.add_subplot(224)
+        self.ax_left = self.fig.add_subplot(223)
+        self.ax_right = self.fig.add_subplot(224)
         self.filepath = None
         self.dist_df = None
         self.temp_df = None
     def replot(self,
                filename=None,
                draw_min_frame = None,
-               draw_max_frame = None):
+               draw_max_frame = None,
+               left_max_pixel = None,
+               right_max_pixel = None,
+               beam_diam_pixel = None):
+        beam_radius_pixel = beam_diam_pixel / 2
         if filename is not None:
             self.filepath = filename
             self.dist_df = pd.read_csv(self.filepath)
         
         # 描画が重ならないように一度消去
         self.ax_main.clear()
+        self.ax_left.clear()
+        self.ax_right.clear()
         
         # フォーマットを実施
         self.temp_df = self.format_dist_df(self.dist_df)
 
         # 全体部分をプロットする
-        
         sns.heatmap(self.temp_df,ax=self.ax_main)
-        self.ax_main.set_ylim(draw_min_frame,draw_max_frame)
-        self.fig.savefig("test2.png")
+        self.ax_main.set_ylim(draw_max_frame,draw_min_frame)
+
+        # レーザー範囲の左側を描画
+        self.ax_main.axvspan(left_max_pixel - beam_radius_pixel,
+                             left_max_pixel + beam_radius_pixel,
+                             color=LASER_RANGE_COLOR,
+                             alpha=LASER_RANGE_ALPHA)
         
+        # レーザー範囲の左側を描画
+        self.ax_main.axvspan(right_max_pixel - beam_radius_pixel,
+                             right_max_pixel + beam_radius_pixel,
+                             color=LASER_RANGE_COLOR,
+                             alpha=LASER_RANGE_ALPHA)
+        
+        # 左側のレーザー範囲近傍を描画
+        sns.heatmap(self.temp_df,ax=self.ax_left)
+        self.ax_left.set_ylim(draw_max_frame,draw_min_frame)
+        self.ax_left.set_xlim(left_max_pixel - CLOSE_SHOW_DIST_PIXEL/2 ,left_max_pixel + CLOSE_SHOW_DIST_PIXEL/2)
+        self.ax_left.axvspan(left_max_pixel - beam_radius_pixel,
+                             left_max_pixel + beam_radius_pixel,
+                             color=LASER_RANGE_COLOR,
+                             alpha=LASER_RANGE_ALPHA)
+        
+        # 右側のレーザー範囲近傍を描画
+        sns.heatmap(self.temp_df,ax=self.ax_right)
+        self.ax_right.set_ylim(draw_max_frame,draw_min_frame)
+        self.ax_right.set_xlim(right_max_pixel - CLOSE_SHOW_DIST_PIXEL/2 ,right_max_pixel + CLOSE_SHOW_DIST_PIXEL/2)
+        self.ax_right.axvspan(right_max_pixel - beam_radius_pixel,
+                              right_max_pixel + beam_radius_pixel,
+                              color=LASER_RANGE_COLOR,
+                              alpha=LASER_RANGE_ALPHA)
     @classmethod
     def format_dist_df(cls,dist_df):
         temp_df = dist_df.drop(['ROI','Frame','Wavelength'], axis=1)
@@ -55,7 +92,13 @@ class plotDistColorMapControl:
         return temp_df
     
 if __name__ == '__main__':
+    # テストを実施
     filename = "model_data/rotated_(-4e-1)ERLAMBDAFeO06_  02(v3.0)_dist(20).csv"
     plot_dist_color_map = plotDistColorMapControl()
-    plot_dist_color_map.replot(filename=filename,draw_min_frame=100,draw_max_frame=200)
-
+    plot_dist_color_map.replot(filename=filename,
+                               draw_min_frame=10,
+                               draw_max_frame=250,
+                               left_max_pixel= 120,
+                               right_max_pixel= 360,
+                               beam_diam_pixel= 7.8125)
+    plt.show()
